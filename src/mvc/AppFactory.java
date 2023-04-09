ac6918d05553e95bf7018d41f01f6bc1d1adc3af
Daniel <dychang124@gmail.com>
2023-04-08 18:10:13 -0700

    Added mvc

---
+++ src/mvc/AppFactory.java
@@ -0,0 +1,12 @@
+package mvc;
+
+public interface AppFactory {
+
+    Model makeModel();
+    View makeView(Model model);
+    Command makeEditCommand(Model model, String type, Object source);
+    String[] getEditCommands();
+    String getTitle();
+    String[] getHelp();
+    String about();
+}
