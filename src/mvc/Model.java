57ec9424ea7762a8ef0dd58c2f54b46675b418a5
Daniel <dychang124@gmail.com>
2023-04-08 18:10:45 -0700

    Added mvc

---
+++ src/mvc/Model.java
@@ -0,0 +1,27 @@
+package mvc;
+
+public class Model extends Bean {
+    private String fileName;
+    private Boolean unsavedChanges;
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public Boolean getUnsavedChanges() {
+        return unsavedChanges;
+    }
+
+    public void setUnsavedChanges(Boolean unsavedChanges) {
+        this.unsavedChanges = unsavedChanges;
+    }
+
+    public void changed() {
+        unsavedChanges = true;
+        firePropertyChange(fileName, false, true);
+    }
+}
