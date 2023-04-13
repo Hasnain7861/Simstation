628d04a932b691760608204c107831322d74c5c6
Nathan Duong <100316459+natooni@users.noreply.github.com>
2023-04-12 21:11:29 -0700

    updated Agent, updated Heading
    Agent:
    added empty methods
    implemented move()
    fixed isSuspended()
    fixed suspend, resume, stop
    
    still need to work on constructor and making sure everything works.
    
    Heading:
    implemented heading. added random() method to return random heading.

--- src/simstation/Agent.java
+++ src/simstation/Agent.java
@@ -13,50 +13,53 @@ public class Agent implements Serializable, Runnable {
     protected Thread myThread;
     protected Simulation mySimulation;
 
-    public Agent() {
+    public Agent(String name) {
+        this.name = name;
         suspended = false;
         stopped = false;
         myThread = null;
     }
 
+    public Agent() {
+        this.name = null;
+        suspended = false;
+        stopped = false;
+        myThread = null;
+    }
+
+    public void onStart() {
+
+    }
+
+    public void onInterrupted() {
+
+    }
+
+    public void onExit() {
+
+    }
+
+
     public void run() {
         myThread = Thread.currentThread();
+        onStart();
+
         while (!isStopped()) {
             try {
                 update();
                 Thread.sleep(1000);
                 checkSuspended();
             } catch(InterruptedException e) {
+                onInterrupted();
                 System.out.println(e.getMessage());
             }
         }
+        onExit();
         System.out.println(name + " stopped");
     }
 
     public synchronized void start() {
 
-    }
-
-    public synchronized void suspend() {
-        suspended = true;
-    }
-
-    public synchronized void resume() {
-        notify();
-    }
-
-    public synchronized void stop() {
-        myThread = Thread.currentThread();
-        while (!isStopped()) {
-            try {
-                update();
-                Thread.sleep(1000);
-                checkSuspended();
-            } catch(InterruptedException e) {
-                System.out.println(e.getMessage());
-            }
-        }
-        System.out.println(name + " stopped");
     }
 
     public synchronized void join() {
@@ -70,16 +73,67 @@ public class Agent implements Serializable, Runnable {
     public void update() {}
 
     public synchronized void move(int steps) {
+        switch(heading) {
 
+            case NORTH: {
+                yc -= steps;
+                if (yc < 0) {
+                    yc = (yc % 500) + 500;
+                }
+
+                break;
+            }
+
+            case EAST: {
+                xc += steps;
+                if (xc > 500) {
+                    xc = (xc % 500);
+                }
+
+                break;
+            }
+
+            case SOUTH: {
+                yc += steps;
+                if (yc > 500) {
+                    yc = (yc % 500);
+                }
+
+                break;
+            }
+
+            case WEST: {
+                xc -= steps;
+                if (xc < 0) {
+                    xc = (xc % 500) + 500;
+                }
+
+                break;
+            }
+
+
+        }
     }
 
-    public void setSimulation(Simulation simulation) {
-        this.mySimulation = simulation;
+    public synchronized void suspend() {
+        suspended = true;
+    }
+
+    public synchronized void resume() {
+        notify();
+    }
+
+    public synchronized void stop() { stopped = true; }
+
+    public void setSimulation(Simulation s) {
+        mySimulation = s;
     }
 
     public boolean isStopped() {
         return stopped;
     }
+
+    public synchronized boolean isSuspended() { return suspended;  }
 
     private synchronized void checkSuspended() {
         try {
@@ -90,5 +144,13 @@ public class Agent implements Serializable, Runnable {
         } catch (InterruptedException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public synchronized String toString() {
+        String result = name;
+        if (stopped) result += " (stopped)";
+        else if (suspended) result += " (suspended)";
+        else result += " (running)";
+        return result;
     }
 }
